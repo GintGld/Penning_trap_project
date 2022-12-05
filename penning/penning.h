0149b3c396a438446754bc91009b93edfd7316c6
Anastasia <pavliuk.aiu@phystech.edu>
2022-12-05 18:57:31 +0300

    put constants in special file

--- penning/penning.h
+++ penning/penning.h
@@ -1,7 +1,9 @@
 #ifndef PENNING_TRAP_MODEL_H
 #define PENNING_TRAP_MODEL_H
 
+// Physic routine
 #include "physic_objects/vector3d.h"
+#include "physic_objects/constants.h"
 #include "physic_objects/particle.h"
 #include "physic_objects/field.h"
 #include "physic_objects/space.h"
---
+++ penning/physic_objects/constants.h
@@ -0,0 +1,24 @@
+#ifndef PHYSICS_CONSTANTS_H
+#define PHYSICS_CONSTANTS_H
+
+////////////////////////////////////////
+//       Fundamental Constants        //
+////////////////////////////////////////
+#define ELEMENTARY_CHARGE 1
+#define ELEMENTARY_MASS  1
+
+
+////////////////////////////////////////
+//             Electron               //
+////////////////////////////////////////
+#define ELECTRON_CHARGE -ELEMENTARY_CHARGE
+#define ELECTRON_MASS ELEMENTARY_MASS
+
+
+////////////////////////////////////////
+//              Proton                //
+////////////////////////////////////////
+#define PROTON_CHARGE ELEMENTARY_CHARGE
+#define PROTON_MASS 1836 * ELEMENTARY_MASS
+
+#endif // PHYSICS_CONSTANTS_H
