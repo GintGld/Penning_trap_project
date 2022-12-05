827e56207a93c32337a874946ac347230f9932b4
Anastasia <pavliuk.aiu@phystech.edu>
2022-12-05 23:26:32 +0300

    function realization

--- penning/physic_objects/field.h
+++ penning/physic_objects/field.h
@@ -1,53 +1,104 @@
 #ifndef FIELD_H
 #define FIELD_H
 
+#include <cmath>
+#include <functional>
+
 #include "vector3d.h"
+#include "constants.h"
+
+////////////////////////////////////////
+//            Declaration             //
+////////////////////////////////////////
 
 template<typename T>
 struct field final
 {
 private:
-    // Function that defines vector field
-    vector3d<T> (*f)(vector3d<T>);
+    std::function<vector3d<T>(vector3d<T>)> f;
 
-    // constructor
-    field(vector<T> (*f)(vector<T>) = [](vector3d<T> r)(return vector3d<T>(0,0,0);));
+    field(std::function<vector3d<T>(vector3d<T>)> f);
 
 public:
-    // changes function
-    void set_function(vector<T> (*f)(vector<T>));
-    
+    void set_function(std::function<vector3d<T>(vector3d<T>)> f_new);
+
     vector3d<T> operator()(vector3d<T> r);
 
-    friend template<typename T>
-    field<T> new_penning_1d();
-
-    friend template<typename T>
-    field<T> new_penning_2d();
-
-    friend template<typename T>
-    field<T> new_penning_3d();
-
-    friend template<typename T>
-    field<T> new_field(vector3d<T> (*f)(vector3d<T>) = [](vector3d<T> r)(return vector3d<T>(0,0,0);));
+    template<typename T_cust>
+    friend field<T_cust> new_field(std::function<vector3d<T_cust>(vector3d<T_cust>)> f);
 };
 
-/*
-    Definitions of Penning Trap fields (2 or 3) 
-    OR
-    Functions that construct these fields
-*/
+////////////////////////////////////////
+//             Definition             //
+////////////////////////////////////////
 
 template<typename T>
-field<T> new_penning_1d();
+field<T>::field(std::function<vector3d<T>(vector3d<T>)> f):
+    f(f) {}
 
 template<typename T>
-field<T> new_penning_2d();
+void field<T>::set_function(std::function<vector3d<T>(vector3d<T>)> f_new)
+{
+    this->f = f_new;
+    return;
+}
 
 template<typename T>
-field<T> new_penning_3d();
+vector3d<T> field<T>::operator()(vector3d<T> r)
+{
+    return f(r);
+}
 
 template<typename T>
-field<T> new_field(vector3d<T> (*f)(vector3d<T>) = [](vector3d<T> r)(return vector3d<T>(0,0,0);));
+field<T> new_field(std::function<vector3d<T>(vector3d<T>)> f)
+{
+    field<T> tmp(f);
+    return tmp;
+}
+
+template<typename T>
+field<T> new_field(vector3d<T> r)
+{
+    return new_field<T>([r](vector3d<T> v){return r;});
+}
+
+template<typename T>
+field<T> new_field()
+{
+    return new_field<T>(vector3d<T>(0,0,0));
+}
+
+template<typename T>
+field<T> penning_E(T C, T eps)
+{
+    field<T> tmp = new_field<T>();
+    tmp.set_function([C, eps](vector3d<T> r){
+        return C * vector3d<T>(
+            (1 + eps) * r.x(),
+            (1 - eps) * r.y(),
+            - 2 * r.z());
+    });
+    return tmp;
+}
+
+template<typename T>
+field<T> penning_M(T B, T theta, T phi)
+{
+    field<T> tmp = new_field<T>();
+    tmp.set_function([B, theta, phi](vector3d<T> r){
+        return B * vector3d<T>(
+            sin(theta) * cos(phi),
+            sin(theta) * cos(phi),
+            cos(theta)
+        );
+    });
+    return tmp;
+}
+
+template<typename T>
+field<T> penning_M(vector3d<T> B)
+{
+    return new_field(B);
+}
 
 #endif // FIELD_H
