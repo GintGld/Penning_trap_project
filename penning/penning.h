7eeb8e4be85460de8fecad0feaaf71e82c7ca623
GintGld <gliaudelis@gmail.com>
2022-12-07 19:22:57 +0300

    added ODE solving

--- penning/penning.h
+++ penning/penning.h
@@ -3,6 +3,8 @@
 
 // Physic routine
 #include "physic_objects/vector3d.h"
+#include "physic_objects/solving_methods.h"
+#include "physic_objects/narray.h"
 #include "physic_objects/constants.h"
 #include "physic_objects/particle.h"
 #include "physic_objects/field.h"
